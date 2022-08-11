83ab789642feb7393c0a895e0e3b8d8a3530ab28
Richard Crockford <richard.crockford@gmail.com>
2022-08-11 09:38:36 +0100

    Cleanup some files

--- flight/tunesteeringsrc.ks
+++
@@ -1,7 +0,0 @@
-steeringmanager:resettodefault().
-set steeringmanager:maxstoppingtime to 2.
-set steeringmanager:PitchPID:kD to 1.
-set steeringmanager:YawPID:kD to 1.
-set steeringmanager:TorqueEpsilonMin to 0.
-set steeringmanager:TorqueEpsilonMax to 1e-5.
-set steeringmanager:RollControlAngleRange to 5.
--- strip_all.bat
+++ strip_all.bat
@@ -12,6 +12,5 @@ kOSStrip reentry\ReEntryBurnSrc.ks reentry\ReEntryBurn.ks
 kOSStrip reentry\ReEntryRCSSrc.ks reentry\ReEntryRCS.ks
 kOSStrip reentry\ReEntryRCSProSrc.ks reentry\ReEntryRCSPro.ks
 kOSStrip reentry\ReEntryLandingSrc.ks reentry\ReEntryLanding.ks
-kOSStrip flight\TuneSteeringSrc.ks flight\TuneSteering.ks
 
 kOSStrip lander\DirectDescent.ks lander\DDPack.ks
